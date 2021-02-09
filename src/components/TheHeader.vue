8abe2e2ac52d354f965624213960792b31cb1c1b
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-02-09 00:02:40 -0500

    Remove background from close button

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -78,7 +78,7 @@
               <button
                 @click="toggleMenu()"
                 type="button"
-                class="bg-white rounded-full p-3 inline-flex items-center justify-center text-gray-400
+                class="rounded-full p-3 inline-flex items-center justify-center text-gray-400
                 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset
                 focus:ring-indigo-500">
                 <span class="sr-only">Close menu</span>
