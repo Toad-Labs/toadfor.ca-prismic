1b19f7c890a71e17b132e47c7c08027cec06ab5a
Neil Mispelaar <neil.mispelaar@outlook.com>
2021-03-09 15:43:22 -0500

    Fix missing reference

--- src/views/ContentPage.vue
+++ src/views/ContentPage.vue
@@ -14,8 +14,6 @@
 <script>
 import { reactive, toRefs, watch } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
-import { apiEndpoint as prismicApiEndpoint } from './../services/prismic-config';
-import Prismic from '@prismicio/client';
 
 export default {
   setup() {
@@ -44,35 +42,6 @@ function usePrismicPageContent() {
 
   // Get the current page parameter
   state.uid = useRoute().params.uid;
-
-
-
-
-  // Check if the uid is a valid page from prismic
-  Prismic.client(prismicApiEndpoint).query([
-    Prismic.Predicates.at('document.id', state.uid),
-    Prismic.Predicates.at('document.type', 'page')
-  ])
-    .then(response => {
-      // response is the response object, response.results holds the documents
-
-      // Check if a result was found
-      if(response.results.length === 0) {
-        state.found = false;
-        console.log('Fail!', state.found);
-      }
-      else {
-        console.log('Found:', response);
-        //state.documentId = response.results[0].id;
-        //state.slices = response.results[0].data.page_content;
-      }
-
-    })
-    .catch((errorResponse) => {
-      console.log('Error', errorResponse);
-      // state.error = errorResponse;
-    });
-
 
   // Watch function to track the found state
   // If found is false forward user to the not-found page
